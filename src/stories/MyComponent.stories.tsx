23799ca2f4108652f9c7159d9050ec0b2253eb4c
Pato <pevargas2730@gmail.com>
2023-12-22 10:17:14 -0600

    template ready

--- src/stories/MyComponent.stories.tsx
+++ src/stories/MyComponent.stories.tsx
@@ -6,7 +6,7 @@ const meta = {
   title: 'Example/MyComponent',
   component: MyComponent,
   argTypes: {
-    data: [],
+
   },
 } satisfies Meta<typeof MyComponent>;
 
@@ -15,27 +15,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    data: [
-        {
-            id: 1,
-            name: "Pato",
-            amount: 200
-        },
-        {
-            id: 2,
-            name: "Jose",
-            amount: -500
-        },
-        {
-            id: 3,
-            name: "Lucas",
-            amount: 300
-        },
-        {
-            id: 4,
-            name: "Patricio",
-            amount: 700
-        }
-    ],
+
   },
 };
--- src/stories/MyComponent.tsx
+++ src/stories/MyComponent.tsx
@@ -1,47 +1,7 @@
-import { useState, useEffect, useCallback } from 'react';
-
 interface MyComponentProps {
-  data: Merchant[];
 }
 
-export interface Merchant {
-  name: string;
-  amount: number;
-  id: number;
-}
+export const MyComponent = ({ }: MyComponentProps) => {
 
-export const MyComponent = ({ data }: MyComponentProps) => {
-  const [filter, setFilter] = useState('');
-  const [filteredData, setFilteredData] = useState<Merchant[]>(data);
-
-  const filterData = useCallback(
-    (data: Merchant[], filter: string): Merchant[] => 
-    data.filter((item) => item.name.toLowerCase().includes(filter)),
-    []
-  );
-
-  useEffect(() => {
-    setFilteredData(filterData(data, filter));
-  }, [data, filter, filterData]);
-
-  const total = (data: Merchant[]): number => data.reduce((sum, { amount }) => sum + amount, 0);
-
-  return (
-    <>
-      <input
-        type="text"
-        placeholder="Filter Data"
-        onChange={(e) => setFilter(e.target.value.toLowerCase())}
-      />
-      <ul>
-        {filteredData.map(({ name, id, amount }) => (
-          <li key={id}>
-            <span>{name}</span>
-            <span style={{ color: `${amount > 0 ? 'green' : 'red'}` }}> {amount}</span>
-          </li>
-        ))}
-      </ul>
-      <p>Total: {total(filteredData)}</p>
-    </>
-  );
+  return ()
 };
