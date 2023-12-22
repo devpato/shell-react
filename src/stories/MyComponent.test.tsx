d74761952e96f05e7ad3b998d848c8dd4ac914b1
Pato <pevargas2730@gmail.com>
2023-12-22 10:57:10 -0600

    my component v2

--- src/stories/MyComponent.test.tsx
+++ src/stories/MyComponent.test.tsx
@@ -2,7 +2,7 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import { MyComponent, Merchant } from './MyComponent';
 import '@testing-library/jest-dom'
 
-describe('Filter component', () => {
+describe('My component', () => {
   const mockData: Merchant[] = [
     {
         id: 1,
@@ -26,42 +26,32 @@ describe('Filter component', () => {
     }
 ]
 
-  it('renders the component with initial data', () => {
-    render(<MyComponent data={mockData} />);
-    
-    // Check if the input and list elements are present
-    const inputElement = screen.getByPlaceholderText('Filter Data');
-    const listElement = screen.getByRole('list');
-    
+  it('Render component with initial data', () => {
+    render(<MyComponent data={mockData}/>);
+
+    const inputElement = screen.getByPlaceholderText("Filter Data");
+    const listElement = screen.getByRole("list");
     expect(inputElement).toBeInTheDocument();
     expect(listElement).toBeInTheDocument();
 
-    // Check if the initial data is rendered
-    mockData.forEach(({ name }) => {
-      const listItem = screen.getByText(name);
-      expect(listItem).toBeInTheDocument();
-    });
-
-    // Check if the total is displayed correctly
-    const totalElement = screen.getByText('Total: 700');
-    expect(totalElement).toBeInTheDocument();
-    expect(screen.getByText('Total: 700')).toBeInTheDocument(); // Assuming the initial total is 600
+    mockData.forEach(({name})=>{
+      const listElement = screen.getByText(name);
+      expect(listElement).toBeInTheDocument();
+    })
   });
 
-  it('filters data based on input', () => {
-    render(<MyComponent data={mockData} />);
+  it('filter data based on input', () => {
+    render(<MyComponent data={mockData}/>);
 
-    const inputElement = screen.getByPlaceholderText('Filter Data');
+    const inputElement = screen.getByPlaceholderText("Filter Data");
+    expect(inputElement).toBeInTheDocument();
 
-    // Type 'Merchant 1' in the input
-    fireEvent.change(inputElement, { target: { value: 'Pato' } });
-
-    // Check if only 'Merchant 1' is displayed
+    fireEvent.change(inputElement, {target:{ value: "Pato"}});
     expect(screen.getByText('Pato')).toBeInTheDocument();
     expect(screen.queryByText('Jose')).not.toBeInTheDocument();
-    expect(screen.queryByText('Patricio')).not.toBeInTheDocument();
 
-    // Check if the total is updated based on the filtered data
-    expect(screen.getByText('Total: 200')).toBeInTheDocument(); // Assuming 'Merchant 1' has an amount of 100
+    const totalElement = screen.getByText('Total: $200');
+    expect(totalElement).toBeInTheDocument();
   });
+
 });
--- src/stories/MyComponent.tsx
+++ src/stories/MyComponent.tsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
 interface MyComponentProps {
+  data: Merchant[]
 }
 
-export const MyComponent = ({ }: MyComponentProps) => {
+export interface Merchant {
+  id: number,
+  name: string,
+  amount: number
+}
 
-  return ()
+export const MyComponent = ({ data }: MyComponentProps) => {
+  const [filter, setFilter] = useState("")
+  const filteredData = data.filter(({name, amount})=> ((`${name} ${amount}`).toLowerCase().includes(filter)));
+  const total = filteredData.reduce((sum, {amount})=> sum + amount, 0);
+
+
+  return (
+    <>
+      <input placeholder="Filter Data" onChange={(e)=>setFilter(e.target.value.toLowerCase()) } />
+      <ul>
+        {
+          filteredData.map(({id, name, amount})=>{
+            return (
+              <li key={id}>
+                  <span>{name}</span>
+                  <span> ${amount}</span>
+              </li>
+            )
+          }
+        )}
+      </ul>
+      <span>Total: ${total}</span>
+    </>
+  )
 };
