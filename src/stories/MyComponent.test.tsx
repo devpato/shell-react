0924e5501cdf13da49fd4bbc0d4094491d3d725a
Pato <pevargas2730@gmail.com>
2023-12-22 11:25:15 -0600

    blank

--- src/stories/MyComponent.test.tsx
+++ src/stories/MyComponent.test.tsx
@@ -1,57 +1,12 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { MyComponent, Merchant } from './MyComponent';
 import '@testing-library/jest-dom'
 
 describe('My component', () => {
-  const mockData: Merchant[] = [
-    {
-        id: 1,
-        name: "Pato",
-        amount: 200
-    },
-    {
-        id: 2,
-        name: "Jose",
-        amount: -500
-    },
-    {
-        id: 3,
-        name: "Lucas",
-        amount: 300
-    },
-    {
-        id: 4,
-        name: "Patricio",
-        amount: 700
-    }
-]
 
-  it('Render component with initial data', () => {
-    render(<MyComponent data={mockData}/>);
+  it('', () => {
+    render(<MyComponent />);
 
-    const inputElement = screen.getByPlaceholderText("Filter Data");
-    const listElement = screen.getByRole("list");
-    expect(inputElement).toBeInTheDocument();
-    expect(listElement).toBeInTheDocument();
-
-    mockData.forEach(({name})=>{
-      const listElement = screen.getByText(name);
-      expect(listElement).toBeInTheDocument();
-    })
-  });
-
-  it('filter data based on input', () => {
-    render(<MyComponent data={mockData}/>);
-
-    const inputElement = screen.getByPlaceholderText("Filter Data");
-    expect(inputElement).toBeInTheDocument();
-
-    fireEvent.change(inputElement, {target:{ value: "Pato"}});
-    expect(screen.getByText('Pato')).toBeInTheDocument();
-    expect(screen.queryByText('Jose')).not.toBeInTheDocument();
-
-    const totalElement = screen.getByText('Total: $200');
-    expect(totalElement).toBeInTheDocument();
   });
 
 });
--- src/stories/MyComponent.tsx
+++ src/stories/MyComponent.tsx
@@ -1,37 +1,10 @@
-import { useState } from "react";
 
 interface MyComponentProps {
-  data: Merchant[]
 }
 
-export interface Merchant {
-  id: number,
-  name: string,
-  amount: number
-}
-
-export const MyComponent = ({ data }: MyComponentProps) => {
-  const [filter, setFilter] = useState("")
-  const filteredData = data.filter(({name, amount})=> ((`${name} ${amount}`).toLowerCase().includes(filter)));
-  const total = filteredData.reduce((sum, {amount})=> sum + amount, 0);
+export const MyComponent = ({ }: MyComponentProps) => {
 
 
   return (
-    <>
-      <input placeholder="Filter Data" onChange={(e)=>setFilter(e.target.value.toLowerCase()) } />
-      <ul>
-        {
-          filteredData.map(({id, name, amount})=>{
-            return (
-              <li key={id}>
-                  <span>{name}</span>
-                  <span> ${amount}</span>
-              </li>
-            )
-          }
-        )}
-      </ul>
-      <span>Total: ${total}</span>
-    </>
   )
 };
