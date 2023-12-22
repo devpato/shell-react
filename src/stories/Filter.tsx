2154296af352888d0461ed17e4db640c3357c647
Pato <pevargas2730@gmail.com>
2023-12-22 00:55:46 -0600

    v2

--- src/stories/Filter.tsx
+++ src/stories/Filter.tsx
@@ -1,38 +1,44 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface FilterProps {
-  data: Merchant[]
+  data: Merchant[];
 }
 
 export interface Merchant {
-    name: string,
-    amount: number,
-    id: number
+  name: string;
+  amount: number;
+  id: number;
 }
 
-export const Filter = ({
-  data,
-}: FilterProps) => {
-    console.log(data)
-    const [filter, setFilter] = useState("")
-    const dataFilter = data.filter(item => item.name.toLowerCase().includes(filter));
-    const total = dataFilter.reduce((sum, {amount})=> sum + amount, 0);
-  
+export const Filter = ({ data }: FilterProps) => {
+  const [filter, setFilter] = useState('');
+  const [filteredData, setFilteredData] = useState<Merchant[]>(data);
+
+  useEffect(() => {
+    const fData = data.filter((item) => item.name.toLowerCase().includes(filter));
+    setFilteredData(fData);
+  }, [data, filter]);
+
+  const total = (data: Merchant[]): number => data.reduce((sum, { amount }) => sum + amount, 0);
+
   return (
     <>
-        <input type='text' placeholder='Filter Data' 
-        onChange={(e)=> setFilter(e.target.value.toLowerCase())}/>
-        <ul>
-            {dataFilter.map(({name, id, amount},)=> {
-                return (
-                    <li key={id}>
-                        <span>{name}</span>
-                        <span style={{color: `${amount > 0 ? 'green' : 'red' }`}}> {amount}</span>
-                    </li>
-                )
-            })}
-        </ul>
-        <p>Total: {total}</p>
+      <input
+        type="text"
+        placeholder="Filter Data"
+        onChange={(e) => setFilter(e.target.value.toLowerCase())}
+      />
+      <ul>
+        {filteredData.map(({ name, id, amount }) => {
+          return (
+            <li key={id}>
+              <span>{name}</span>
+              <span style={{ color: `${amount > 0 ? 'green' : 'red'}` }}> {amount}</span>
+            </li>
+          );
+        })}
+      </ul>
+      <p>Total: {total(filteredData)}</p>
     </>
   );
 };
