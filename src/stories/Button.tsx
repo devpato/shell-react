d0385dd301c8d0131ce609ab427a2d43b03c4c43
Pato <pevargas2730@gmail.com>
2023-12-22 00:12:49 -0600

    scss modules working

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,32 +1,13 @@
-import React from 'react';
-import './button.css';
+import styles from './button.module.scss';
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
   primary?: boolean;
-  /**
-   * What background color to use
-   */
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
   size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
   label: string;
-  /**
-   * Optional click handler
-   */
   onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
 export const Button = ({
   primary = false,
   size = 'medium',
@@ -38,7 +19,7 @@ export const Button = ({
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={`${styles['storybook-button']} ${styles[`storybook-button--${size}`]} ${styles[mode]}`}
       style={{ backgroundColor }}
       {...props}
     >
